6a178d30dd737b8ca030d94e7df0518a0f0555f5
Zach Riane Machacon <zachriane01@gmail.com>
2024-10-10 10:52:57 +0800

    fix: fixed wezterm font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,15 +8,12 @@ local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
 end
 
-local is_darwin = function()
-	return wezterm.target_triple:find("darwin") ~= nil
-end
-
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
 config.color_scheme = "Dracula"
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+config.font_size = is_linux() and 10.0 or 12.0
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
 config.enable_scroll_bar = true
