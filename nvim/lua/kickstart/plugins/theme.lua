03e11a6d5133ff9d9f56914addf5fd70c7ecf8f9
Zach Riane Machacon <zachriane01@gmail.com>
2025-01-07 17:34:32 +0800

    feat: reverted transparency

--- nvim/lua/kickstart/plugins/theme.lua
+++ nvim/lua/kickstart/plugins/theme.lua
@@ -6,12 +6,6 @@ return {
       vim.cmd.colorscheme 'dracula'
       vim.cmd.hi 'Comment gui=none'
     end,
-    config = function()
-      local dracula = require 'dracula'
-      dracula.setup {
-        transparent_bg = true,
-      }
-    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
