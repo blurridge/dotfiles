232f296c70fdaddd1443a8fd110a8b45f17e2060
Zach Riane Machacon <zachriane01@gmail.com>
2026-02-25 15:41:40 +0800

    added resolve

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -60,7 +60,9 @@ require('lazy').setup({
 
   require 'custom/plugins/noice',
 
-  require 'custom/plugins/git-blame'
+  require 'custom/plugins/git-blame',
+
+  require 'custom/plugins/resolve'
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
