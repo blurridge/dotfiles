ca7f4891117bfc0b7d03d88ce1edbe40221fa256
Zach Riane Machacon <zachriane01@gmail.com>
2026-03-02 14:44:42 +0800

    [Fix][Zach] Added virtual text for errors

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -29,6 +29,25 @@ return {
       'saghen/blink.cmp',
     },
     config = function()
+      vim.diagnostic.config {
+        virtual_text = {
+          enabled = true,
+          spacing = 4,
+          source = 'if_many',
+          prefix = '●',
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          focusable = false,
+          style = 'minimal',
+          border = 'rounded',
+          source = true,
+        },
+      }
+
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
