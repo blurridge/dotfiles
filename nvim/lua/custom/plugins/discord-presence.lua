9f6457d39bca2071eba0c304a35218f16aa257f8
Zach Riane Machacon <zachriane01@gmail.com>
2026-03-04 13:59:43 +0800

    ts icon

--- nvim/lua/custom/plugins/discord-presence.lua
+++ nvim/lua/custom/plugins/discord-presence.lua
@@ -1,26 +1,35 @@
 return {
   {
     'vyfor/cord.nvim',
-    opts = {
-      editor = {
-        tooltip = 'Struggling with vim help me',
-      },
-      display = {
-        theme = 'catppuccin',
-        flavor = 'accent',
-        swap_fields = false,
-        swap_icons = false,
-      },
-      timestamp = {
-        enabled = true,
-        reset_on_idle = false,
-        reset_on_change = false,
-      },
-      idle = {
-        enabled = true,
-        timeout = 300000,
-        show_status = true,
-      },
-    },
+    config = function()
+      local icon = require('cord.api.icon').get
+      require('cord').setup {
+        editor = {
+          tooltip = 'Struggling with vim help me',
+        },
+        display = {
+          theme = 'catppuccin',
+          flavor = 'accent',
+          swap_fields = false,
+          swap_icons = false,
+        },
+        timestamp = {
+          enabled = true,
+          reset_on_idle = false,
+          reset_on_change = false,
+        },
+        idle = {
+          enabled = true,
+          timeout = 300000,
+          show_status = true,
+        },
+        assets = {
+          ['.service.ts'] = { icon = icon('typescript'), tooltip = 'TypeScript' },
+          ['.module.ts'] = { icon = icon('typescript'), tooltip = 'TypeScript' },
+          ['.guard.ts'] = { icon = icon('typescript'), tooltip = 'TypeScript' },
+          ['.interceptor.ts'] = { icon = icon('typescript'), tooltip = 'TypeScript' },
+        },
+      }
+    end,
   },
 }
